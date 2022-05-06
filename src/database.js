4bed0cd5cf9d7fa6b81a29280709a35ed03dd53d
Misaianes <sairebautista@gmail.com>
2022-05-06 01:41:59 -0300

    ruta de datos de base expuesta

--- src/database.js
+++ src/database.js
@@ -1,6 +1,7 @@
 require("dotenv").config()
 const mongoose = require("mongoose");
-const URI = process.env.MONGO_ATLAS;
+//const URI = process.env.MONGO_ATLAS;
+const URI = "mongodb+srv://Misaianes:srdinero741@cluster0.rluxc.mongodb.net/mernStackTask?retryWrites=true&w=majority";
 mongoose.connect(URI)
 .then(db=>{
     console.log("DB is connected")
